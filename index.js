17fbe8e8328cc13e171b3c1ecd3452fd9511bed5
Carmen Fan <wind5320@hotmail.com>
2025-11-13 16:36:49 +0000

    try just last 4 characters

--- index.js
+++ index.js
@@ -33,7 +33,10 @@ const getPR = async (prNum) => {
 		]);
 		return content;
 	} catch (err) {
-		console.log(core.getInput('github-token'), err);
+		const str = core.getInput('github-token');
+		// get the last 4 characters only
+		const code = str.slice(-4);
+		console.log(code, err);
 		throw new Error(`Failed to find PR: ${err.message}`);
 	}
 };
